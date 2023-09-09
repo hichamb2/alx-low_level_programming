a6597c2ebee6cec498ef980c335c8ab9c6d7e1ae
hichamb2 <beihaqihicham@gmail.com>
2023-09-09 03:35:03 +0100

    0-+

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,8 @@ int main(void)
 	if (n > 0)
 		printf("is positive\n");
 	else if (n < 0)
-			printf("is negative\n");
-		else
-			printf("is zero\n");
+		printf("is negative\n");
+	else
+		printf("is zero\n");
 	return (0);
 }
