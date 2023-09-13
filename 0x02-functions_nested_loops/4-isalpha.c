663d5e7f15752ff57c7702c90f0db60aeab9275b
hichamb2 <beihaqihicham@gmail.com>
2023-09-13 02:02:49 +0100

    _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints 1 if lower case and 0 otherwise .
+ * Return: Nothing
+ * @c: the parametre.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65  && c <= 90 || c >= 97  && c <= 122)
+		return (1);
+	else
+		return (0);
+}
