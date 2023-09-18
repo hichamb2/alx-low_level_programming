5557f1e0fdfec95131ffe936a0b917d923df0f2e
hichamb2 <beihaqihicham@gmail.com>
2023-09-18 21:35:57 +0100

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that reset any number to 98
+ *
+ * @n: pointer that point on parametre
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is description about 0x05-pointers_arrays_strings
