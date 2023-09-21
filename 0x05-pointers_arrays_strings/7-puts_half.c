bde64c5c0135b9881b6917f5441c33402382c7f0
hichamb2 <beihaqihicham@gmail.com>
2023-09-21 21:35:41 +0100

    7 half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - function
+ * @str: the parametr
+ *
+ */
+void puts_half(char *str)
+{
+int len, i = 0;
+while (str[i] != '\0')
+i++;
+if (i % 2 == 0)
+	len = i / 2;
+else
+	len = (i - 1) / 2;
+for (len = len; len <= i; len++)
+{
+	_putchar(str[len]);
+}
+_putchar('\n');
+}
