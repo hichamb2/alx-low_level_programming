1ddcd924f87fe7d6dcebe2ea0ba7dab955c8784f
hichamb2 <beihaqihicham@gmail.com>
2023-09-22 00:14:27 +0100

    tl

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	char *ret = dest;
 
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 	dest[i + 1] = '\0';
-	return (dest);
+	return (ret);
 }
