95049fb438f3a49d883cce5382d2eb06491732a9
hichamb2 <beihaqihicham@gmail.com>
2023-09-25 18:09:58 +0100

    add

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,16 +9,20 @@ char *cap_string(char *ptr)
 {
 	int i;
 
-	for (i = 0; ptr[i] != '\0'; i++)
+for (i = 0; ptr[i] != '\0'; i++)
+{
+	while (ptr[i + 1] > 96 && ptr[i + 1] < 123)
 	{
-		if ((ptr[i] == ' ' || ptr[i] == '\n' ||
+		if (ptr[i] == ' ' || ptr[i] == '\n' ||
 		ptr[i] == '\t' || ptr[i] == ',' ||
 		ptr[i] == ';' || ptr[i] == '.' ||
 		ptr[i] == '!' || ptr[i] == '?' ||
 		ptr[i] == '"' || ptr[i] == '(' ||
 		ptr[i] == ')' || ptr[i] == '{' ||
-		ptr[i] == '}') && (ptr[i + 1] > 96 && ptr[i + 1] < 123))
+		ptr[i] == '}')
 			ptr[i + 1] -= 32;
+	i++;
 	}
+}
 	return (ptr);
 }
