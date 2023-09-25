b49a89fd78d88617b17b677b90001d90c7dc07d3
hichamb2 <beihaqihicham@gmail.com>
2023-09-25 17:45:07 +0100

    add

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *cap_string - function low toupp
+ * @ptr: 1st parametre
+ * Return: char to pointer
+ *
+ */
+char *cap_string(char *ptr)
+{
+	int i;
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		if ((ptr[i] == ' ' || ptr[i] == '\n' ||
+		ptr[i] == '\t' || ptr[i] == ',' ||
+		ptr[i] == ';' || ptr[i] == '.' ||
+		ptr[i] == '!' || ptr[i] == '?' ||
+		ptr[i] == '"' || ptr[i] == '(' ||
+		ptr[i] == ')' || ptr[i] == '{' ||
+		ptr[i] == '}') && (ptr[i + 1] > 96 && ptr[i + 1] < 123))
+			ptr[i + 1] -= 32;
+	}
+	return (ptr);
+}
