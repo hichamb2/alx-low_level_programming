72e06e17a65d6cbe44400ba93f9f3746aaa41f10
hichamb2 <beihaqihicham@gmail.com>
2023-09-27 14:11:19 +0100

    advance 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *rot13 - Change string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *rot13(char *str)
+{
+	char alp[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char con[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alp[j]; j++)
+		{
+			if (str[i] == alp[j])
+				str[i] = con[j];
+		}
+	}
+	return (str);
+}
