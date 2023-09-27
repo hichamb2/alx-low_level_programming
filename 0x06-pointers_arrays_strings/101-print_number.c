c21b35c8547a62696f0dcf3e53da7e7b02e12774
hichamb2 <beihaqihicham@gmail.com>
2023-09-27 17:32:01 +0100

    print_numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - Function print number
+ * @n: input parametre
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10 > 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
