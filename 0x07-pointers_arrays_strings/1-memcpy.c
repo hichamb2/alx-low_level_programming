a10d0bc8ca148df3f7157b35f5a90a04d113c5a4
hichamb2 <beihaqihicham@gmail.com>
2023-09-28 12:49:17 +0100

    add

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - function
+ * @dest: the buffer
+ * @src: the character
+ * @n: the size of the buffer
+ * Return: prt on s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *ptr = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		*(src + i) = *(dest + i);
+	}
+	return (ptr);
+}
