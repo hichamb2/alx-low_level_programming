6804ec7668f4996af29a949cefe3e1d65f5ffc08
hichamb2 <beihaqihicham@gmail.com>
2023-10-03 00:27:56 +0100

    add

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strstr - the function
+ * @haystack: first par
+ * @needle: seconde par
+ * Return: tha firsy occurance of accept
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int h = 0, con = 0;
+	int hh, n, an;
+	char *ptr;
+
+	while (needle[con])
+		con++;
+	while (haystack[h] != '\0')
+	{
+		if (haystack[h] == needle[0])
+		{
+			ptr = &haystack[h];
+			hh = h;
+			an = 0;
+			for (n = 0; n < con; n++)
+			{
+				if (haystack[hh] == needle[n])
+					an++;
+				hh++;
+			}
+		if (an == con)
+			return (ptr);
+		}
+	h++;
+	}
+	return ('\0');
+}
