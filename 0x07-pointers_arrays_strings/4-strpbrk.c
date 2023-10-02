d79d76506eb4c2578065faf94a2cfd1993d7bb74
hichamb2 <beihaqihicham@gmail.com>
2023-10-02 23:19:36 +0100

    file 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strpbrk - the function
+ * @s: first par
+ * @accept: seconde par
+ * Return: tha firsy occurance of accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		while (accept[i] != '\0')
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		i++;
+		}
+	s++;
+	i = 0;
+	}
+	return ('\0');
+}
