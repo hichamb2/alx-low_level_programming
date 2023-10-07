4b0fa14806dd46eab2b9afe86ba42b222dfb15fd
hichamb2 <beihaqihicham@gmail.com>
2023-10-07 17:23:29 +0100

    a file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = (char*)malloc(size * sizeof(c));
+	ptr = (char *)malloc(size * sizeof(c));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
