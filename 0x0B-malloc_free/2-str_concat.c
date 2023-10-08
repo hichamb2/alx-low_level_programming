098f724a185839993f34fa8747d26a8dbefd1d7d
hichamb2 <beihaqihicham@gmail.com>
2023-10-08 01:15:16 +0100

    add commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,9 +9,11 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j, leng = 0;
 	char *s;
-	
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		leng++;
 	for (i = 0; s2[i] != '\0'; i++)
