a5b4225bc7ff6ce56920817bda06a5c0fab86b70
hichamb2 <beihaqihicham@gmail.com>
2023-10-12 23:29:22 +0100

    add commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - list dates about pet
- * @name: pet name
- * @age: pet age
- * @owner: pet owner name
+ * struct dog - the structur
+ * @name: name of dog
+ * @age: the age of it
+ * @owner: the owner
+ *
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
 
+#endif
