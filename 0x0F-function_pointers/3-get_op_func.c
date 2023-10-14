0498cc918413c4574379384a75366b5f9cc6554d
hichamb2 <beihaqihicham@gmail.com>
2023-10-14 20:51:48 +0100

    add commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,34 +2,27 @@
 #include <stddef.h>
 
 /**
- * get_op_func - get ops function pointer of type char array
- *               that accepts two inputs of int data type
- *
- * @s: a character pointer pointing to a symbol from the program argument
- *
- * Return: one of the operator functions to perform calculations
-*/
-
+ * get_op_func - function
+ * @s: the operation
+ * Return: an int
+ */
 int (*get_op_func(char *s))(int, int)
 {
-	/* struct opts of struct op_t */
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}};
 	int i = 0;
 
 	while (i < 5)
 	{
+
 		if (*s == *ops[i].op)
 			return (ops[i].f);
-		i++;
+	i++;
 	}
-
 	return (NULL);
 }
-
