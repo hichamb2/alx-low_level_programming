325eae6aad403116c9c8eba70175488d677e01b3
hichamb2 <beihaqihicham@gmail.com>
2023-10-14 01:34:51 +0100

    add commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
 /**
- * print_name - function to print name
+ * print_name - function
+ * @name: strig
+ * @f: pointer to function
  *
- * @name: name to print
- * @f: function pointer that doesn't return anything
- *
- * Return: Always 0 (Success)
-*/
-
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name != NULL && f != NULL)
 		f(name);
 }
