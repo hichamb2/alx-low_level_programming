c1878c7593f1787feda68c919951db660bcdce0d
hichamb2 <beihaqihicham@gmail.com>
2023-10-14 16:22:07 +0100

    add commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, ret;
 
-	if (size <= 0 || cmp == NULL)
+	if (size <= 0)
 		return (-1);
 	if (array != NULL && size > 0 && cmp != NULL)
 	{
