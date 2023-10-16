380637d5d2ef7759055fed80f0b6177388a86894
hichamb2 <beihaqihicham@gmail.com>
2023-10-16 15:06:24 +0100

    add commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+/**
+ * print_all - functio prints numbers
+ * @format: the str
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	char *str;
+	int i = 0;
+	char *sep = ", ";
+
+	va_start(ptr, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(ptr, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ptr, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ptr, double));
+			break;
+		case 's':
+			str = va_arg(ptr, char *);
+			if (str == NULL)
+				printf("(nil)");
+			 printf("%s", str);
+			break;
+		default:
+			break;
+		if (format[i + 1] != '\0')
+			printf("%s", sep);
+	i++;
+	}
+	va_end(ptr);
+	printf("\n");
+}
