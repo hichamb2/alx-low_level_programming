55e30aa060e1de5888577a005d70afa543a385a9
hichamb2 <beihaqihicham@gmail.com>
2023-10-16 01:52:11 +0100

    add commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,11 +17,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 		{
 			printf("%s", "(nil)");
-			break;
 		}
+		else
+		{
 		printf("%s", str);
 		if (separator != NULL && i < (n - 1))
 			printf("%s", separator);
+		}
 	}
 	va_end(ptr);
 	printf("\n");
