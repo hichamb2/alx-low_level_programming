95d29aaeae29a7722e7eb579e502df698ab70777
hichamb2 <beihaqihicham@gmail.com>
2023-10-29 00:52:52 +0100

    add

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	res = 1;
 	def = n ^ m;
-	for (index = 0; index < ((sizeof(unsigned long int)) * 8 - 1); index++)
+	for (index = 0; index < ((sizeof(unsigned long int)) * 8); index++)
 	{
 		if (res == (def & res))
 		{
