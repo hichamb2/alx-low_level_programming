40e3d273259fb2b6b36851028f3e3256f338b305
hichamb2 <beihaqihicham@gmail.com>
2023-10-29 00:18:00 +0100

    add

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -46,6 +46,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > ((sizeof(unsigned long int)) * 8 - 1))
 		return (-1);
 	num = ~(1 << index);
-	*n = *n | num;
+	*n = *n & num;
 	return (1);
 }
