b7474af2945b78c472419c7c8146b9cbb421f95a
hichamb2 <beihaqihicham@gmail.com>
2023-11-05 02:23:59 +0100

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		for (lengh = 0; text_content[lengh] != '\0'; lengh++)
 			;
 	}
-	op = open(filename, O_RDWR | O_CREAT, 0600);
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	wr = write(op, text_content, lengh);
 	if (op == -1 || wr == -1)
 		return (-1);
