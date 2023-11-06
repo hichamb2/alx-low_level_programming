2a55f9cb46ecda5ab0dc989e53aed94364476234
hichamb2 <beihaqihicham@gmail.com>
2023-11-06 01:37:56 +0100

    add

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,17 +16,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (lengh = 0; text_content[lengh] != '\0'; lengh++)
 			;
 	}
-	else
-		return (-1);
 	op = open(filename, O_WRONLY | O_APPEND);
 	if (op == -1)
 		return (-1);
 	if (text_content)
 	{
 		wr = write(op, text_content, lengh);
+		if (wr == -1)
+			return (-1);
 	}
-	if (wr == -1)
-		return (-1);
 	close(op);
 	return (1);
 }
