cdccdc0d58c1488184b125b96656a9c716161f2f
hichamb2 <beihaqihicham@gmail.com>
2023-11-25 13:45:44 +0100

    add

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_list - print function
+ * @h: the list
+ * Return: nul of nds
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	const list_t *ptr = h;
+
+	if (ptr->str == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	else
+	{
+		while (ptr != NULL)
+		{
+			printf("[%u] %s", ptr->len, ptr->str);
+			printf("\n");
+			ptr = ptr->next;
+			n++;
+		}
+	}
+	return (n);
+}
