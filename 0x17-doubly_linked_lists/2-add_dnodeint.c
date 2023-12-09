e6bea465c22623d67247d3c44a42c4dfb61fcd5f
hichamb2 <beihaqihicham@gmail.com>
2023-12-09 17:02:49 +0100

    add

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,9 +9,9 @@
  * Return: pointer to the new node, or NULL on failure
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{	
+{
 	dlistint_t *temp;
-	
+
 	temp = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (temp == NULL)
 		return (NULL);
