13957b91eadf83381908d9ea7903d66d7715bf25
hichamb2 <beihaqihicham@gmail.com>
2024-01-13 23:49:41 +0100

    add

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *
- *
- *
+ * main - check the code
+ * @argc: first par
+ * @argv: second par
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 0)
-		printf("%s\n", argv[0]);
-	return (0);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+		return (0);
 }
