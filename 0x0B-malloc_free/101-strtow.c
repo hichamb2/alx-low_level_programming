6e28c2308a7d2137a0f8863a82212be145827728
hichamb2 <beihaqihicham@gmail.com>
2024-01-17 01:34:04 +0100

    add

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,28 +1,89 @@
 #include "main.h"
+#include <stdlib.h>
+
+void fonc(char **, char *);
+void create_word(char **, char *, int, int, int);
+
 /**
- * *argstostr - function
- * @ac: argc
- * @av: argv
- * Return: ptr
+ * strtow - splits a string into words.
+ * @str: the string
+ *
+ * Return: returns a pointer to an array of strings (words)
  */
 char **strtow(char *str)
 {
-	int i, wc;
-	int *ptr;
+	int i, f, len;
+	char **words;
 
-	if (str == NULL || str == '')
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	i = f = len = 0;
+	while (str[i])
 	{
-		if (str[i] != ' ')
-			wc++;
+		if (f == 0 && str[i] != ' ')
+			f = 1;
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			f = 0;
+			len++;
+		}
+	i++;
 	}
-	if (wc == 0)
-		return (0);
-	ptr = malloc((wc + 1) * sizeof(char));
-	if (ptr == NULL)
+	len += f == 1 ? 1 : 0;
+	if (len == 0)
 		return (NULL);
-	while (*str)
+	words = (char **)malloc(sizeof(char *) * (len + 1));
+	if (words == NULL)
+		return (NULL);
+	fnc(words, str);
+	words[len] = NULL;
+	return (words);
+}
+
+/**
+ * fonc - a fnc function for fetching words into an array
+ * @words: the strings array
+ * @str: the string
+ **/
+void fnoc(char **words, char *str)
+{
+	int i, j, start, f;
+
+	i = j = f = 0;
+	while (str[i])
 	{
+		if (f == 0 && str[i] != ' ')
+		{
+			start = i;
+			f = 1;
+		}
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			create_word(words, str, start, i, j);
+			j++;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 1)
+		create_word(words, str, start, i, j);
+}
 
+/**
+ * create_word - creates a word and insert it into the array
+ * @words: the array of strings
+ * @str: the string
+ * @start: the starting index of the word
+ * @end: the stopping index of the word
+ * @index: the index of the array to insert the word
+ */
+void create_word(char **words, char *str, int start, int end, int index)
+{
+	int i, j;
 
+	i = end - start;
+	words[index] = (char *)malloc(sizeof(char) * (i + 1));
+	for (j = 0; start < end; start++, j++)
+		words[index][j] = str[start];
+	words[index][j] = '\0';
+}
