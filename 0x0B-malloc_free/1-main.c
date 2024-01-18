2ab1e97ad2ecfa97c7414593ebe7a2bc59511022
hichamb2 <beihaqihicham@gmail.com>
2024-01-18 21:37:41 +0100

    add

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -10,26 +10,11 @@
 int main(void)
 {
 char *s;
-s = str_concat("Hello", NULL);
+
+s = _strdup("ALX SE");
 if (s == NULL)
 {
-printf("failed\n");
-return (1);
-}
-printf("%s\n", s);
-free(s);
-s = str_concat(NULL, "Hello");
-if (s == NULL)
-{
-printf("failed\n");
-return (1);
-}
-printf("%s\n", s);
-free(s);
-s = str_concat(NULL, NULL);
-if (s == NULL)
-{
-printf("failed\n");
+printf("failed to allocate memory\n");
 return (1);
 }
 printf("%s\n", s);
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,36 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * *_strdup - function creat array
- * @str: size of the array
- * Return: rerutn the array
+ * _strlen - function that calcul the lengh of string
+ * @str: the str that calculated
+ * Return: the lengh of str
+ */
+int _strlen(char *str)
+{
+	int i, lengh = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		lengh++;
+	return (lengh);
+}
+/**
+ * _strdup - function that returns a copy of the string given as a parameter.
+ * @str: the string wich will be duplicated
+ * Return: a pointer to a newly allocated space in memory
  */
 char *_strdup(char *str)
 {
-	int leng = 0, i;
+	int len, i;
 	char *ptr;
 
-	if (str == NULL)
-		return (NULL);
-	for (; str[leng] != '\0'; leng++)
-		;
-	ptr = malloc((leng + 1) * sizeof(char));
+	len = _strlen(str);
+	if (len == 0)
+		return (0);
+	ptr = (char *)malloc((sizeof(char) * len) + 1);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		ptr[i] = str[i];
-	}
 	return (ptr);
+
 }
