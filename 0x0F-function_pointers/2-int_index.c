534604cfbc651b5dfe6cfe50a4165b3fa6ebe0a1
hichamb2 <beihaqihicham@gmail.com>
2024-01-25 02:57:01 +0100

    add

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,18 +13,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, ret;
-
-	if (size <= 0)
+	int i;
+	
+	if (!array || !cmp || size <= 0)
 		return (-1);
-	if (array != NULL && size > 0 && cmp != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			ret = cmp(array[i]);
-			if (ret)
-				return (i);
-		}
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 	return (-1);
 }
