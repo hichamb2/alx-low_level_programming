fe2b7857be2c0856f0275803c3ba08b0bb4c88c9
hichamb2 <beihaqihicham@gmail.com>
2024-02-05 00:24:09 +0100

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,7 +31,10 @@ list_t *add_node(list_t **head, const char *str)
 	len = _strlen(strdup(str));
 	temp = (list_t *)malloc(sizeof(list_t));
 	if (temp == NULL)
+	{
+		free(len);
 		return (NULL);
+	}
 	temp->str = strdup(str);
 	temp->len = len;
 	temp->next = *head;
