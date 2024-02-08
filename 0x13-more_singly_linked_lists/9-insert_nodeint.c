24b8fbba9a733e3d635077c6e4307480aa8ba0fa
hichamb2 <beihaqihicham@gmail.com>
2024-02-08 21:16:00 +0100

    add

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements
+ *               in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements in the listint_t list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
+/**
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *				list at a given position.
+ * @head: A pointer to the address of the
+ *		head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *		node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *		Otherwise - the address of the new node.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *ptr;
+	size_t i = 0, len = 0;
+
+	len = listint_len(*head);
+	if (*head == NULL || len < idx)
+		return (NULL);
+	ptr = (listint_t *)malloc(sizeof(listint_t));
+	if (!ptr)
+		return (NULL);
+	ptr->n = n;
+	ptr->next = NULL;
+	temp = *head;
+	while (i < idx - 1 && temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	ptr->next = temp->next;
+	temp->next = ptr;
+	return (*head);
+}
