149bfe60232bea174ff215c81f8909ee9295e8fb
hichamb2 <beihaqihicham@gmail.com>
2024-02-09 02:17:10 +0100

    add

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,62 +1,44 @@
 #include "main.h"
 /**
- * _div - calcul the division
- * @x: dividend
- * @y: divisor
- * Return: the result of x / y
- */
-unsigned int _div(unsigned int x, unsigned int y)
-{
-unsigned int digit = 0;
-
-while (x >= y)
-{
-x -= y;
-digit++;
-}
-return (digit);
-}
-/**
- * _power - calculate the power of 2 numbers
- * @base: the base of power num
- * @pow: the power of pow
- * Return: return the result of "base" power "pow"
+ * _power - calculate (base and power)
+ * @base: base of the exponet
+ * @pow: power of the exponet
+ * Return: value of base and power
  */
 unsigned long int _power(unsigned int base, unsigned int pow)
 {
-	unsigned long int result = 1;
+	unsigned long int num;
 	unsigned int i;
 
-	for (i = 0; i < pow; i++)
-	{
-		result *= base;
-	}
-	return (result);
+	num = 1;
+	for (i = 1; i <= pow; i++)
+	num *= base;
+	return (num);
 }
 /**
- * print_binary - convert to binary
- * @n: numero in decimal
+ * print_binary - prints the binary representation of a number
+ * @n: num of prented
+ * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned int digit, num;
-	char bin;
+	unsigned long int dev, result;
+	char flag;
 
-	bin = 0;
-	digit = _power(2, sizeof(unsigned long int) * 8 - 1);
-	while (digit != 0)
+	flag = 0;
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+	while (dev != 0)
 	{
-		num = n & digit;
-		if (num == digit)
+		result = n & dev;
+		if (result == dev)
 		{
-			bin = 1;
+			flag = 1;
 			_putchar('1');
 		}
-		else if (bin == 1 || digit == 1)
+		else if (flag == 1 || dev == 1)
 		{
 			_putchar('0');
 		}
-	digit >>= 1;
+		dev >>= 1;
 	}
 }
