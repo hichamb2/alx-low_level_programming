d4acf4917d0a2a2f000422219d79b6fef861d947
hichamb2 <beihaqihicham@gmail.com>
2024-02-10 23:36:16 +0100

    add

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,9 +19,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		temp = temp->next;
-		ptr = temp->next;
-		free(ptr);
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
 	for (i = 0; i < index - 1; i++)
