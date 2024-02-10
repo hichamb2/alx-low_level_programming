59099f1611f833db2b887e65e0c7b0106372e6e1
hichamb2 <beihaqihicham@gmail.com>
2024-02-10 15:46:44 +0100

    add

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -48,7 +48,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = ptr;
 		return (*head);
 	}
-	if (idx = 0)
+	if (idx == 0)
 	{
 		ptr->next = *head;
 		*head = ptr;
