1ba33a9126187a830d8290413a0e2e48153ca013
hichamb2 <beihaqihicham@gmail.com>
2024-03-21 23:17:52 +0000

    add

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int ret;
+	int ret = 0;
 	dlistint_t *temp;
 
-	temp = h;
+	if (h == NULL)
+		return (ret);
+	temp = (dlistint_t *)h;
 	while (temp != NULL)
 	{
-		ret = printf("%d\n", temp->n);
+		printf("%d\n", temp->n);
+		ret++;
 		temp = temp->next;
 	}
 	return (ret);
