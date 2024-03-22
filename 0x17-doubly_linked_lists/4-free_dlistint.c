b77491f33da4fa7aa4c285a569ae6774b9d9ab37
hichamb2 <beihaqihicham@gmail.com>
2024-03-22 01:58:23 +0000

    add

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,3 +9,23 @@
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp;
+
+	if (head == NULL)
+		return;
+	temp = head;
+	if (temp->next != NULL)
+	{
+		temp = temp->next;
+		while (temp != NULL)
+		{
+			free(head->next), head->next = NULL;
+			free(temp->prev), temp->prev = NULL;
+			head = temp;
+			temp = temp->next;
+		}
+	}
+	free(head), head = NULL;
+	free(temp), temp = NULL;
+}
+
